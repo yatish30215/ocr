9b78d0fcde879883c1d9703e6780aeeb2befb266
Yatish Shirsath <yatish1433@gmail.com>
2022-06-24 17:50:26 +0530

    Updated and released apk

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,6 @@ import 'dart:io' as Io;
 // ignore: unused_import
 import 'package:camera/camera.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:open_file/open_file.dart';
-// import 'globals.dart' as globals;
 
 void main() {
   runApp(const MyApp());
@@ -42,7 +39,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  get globals => null;
+  String globals = '';
 
   @override
   Widget build(BuildContext context) {
@@ -77,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   minimumSize: const Size(100, 50), //////// HERE
                 ),
                 onPressed: () {
-                  pickImage();
+                  clickImage();
                 },
                 icon: const Icon(Icons.camera),
                 label: const Text('Click Photo'),
@@ -93,7 +90,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   minimumSize: const Size(100, 50), //////// HERE
                 ),
                 onPressed: () {
-                  _pickFile();
+                  // _pickFile();
+                  pickImage();
                 },
                 icon: const Icon(Icons.upload),
                 label: const Text('Upload Photo'),
@@ -105,58 +103,17 @@ class _MyHomePageState extends State<MyHomePage> {
             'Your result is:',
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
           ),
-          Text(globals.toString())
+          Text(
+            globals,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          )
         ],
       ),
     );
   }
 
-  Future<void> _pickFile() async {
-    final result = await FilePicker.platform.pickFiles(allowMultiple: true);
-
-    // if no file is picked
-    if (result == null) return;
-
-    // we get the file from result object
-    final file = result.files.first;
-
-    _openFile(file);
-  }
-
-  Future<void> _plateCall(String img64) async {
-    setState(() async {
-      var url = Uri.parse('https://api.platerecognizer.com/v1/plate-reader/');
-      var response = await http.post(url, body: {
-        'upload': img64,
-        'regions': 'in',
-      }, headers: {
-        "Authorization": "Token 497717d5ef388c529bc0eaa84f6924baa3a4acbf"
-      });
-      // ignore: avoid_print
-      print('Response status: ${response.statusCode}');
-      // ignore: avoid_print
-      print('Response body: ${response.body}');
-      final responseJson = json.decode(response.body);
-      // ignore: avoid_print, unnecessary_brace_in_string_interps
-      print('Response Vehicle: ${responseJson["results"][0]["vehicle"]}');
-      // ignore: avoid_print
-      final globals = responseJson["results"][0]["plate"];
-      // ignore: avoid_print
-      print('Response plate: $globals');
-    });
-  }
-
-  Future<void> _openFile(PlatformFile file) async {
-    OpenFile.open(file.path);
-    debugPrint(file.path);
-    final bytes = Io.File(file.path.toString()).readAsBytesSync();
-    String img64 = base64Encode(bytes);
-    debugPrint(file.path.toString());
-    _plateCall(img64);
-  }
-
   Io.File? image;
-  Future pickImage() async {
+  Future clickImage() async {
     try {
       final image = await ImagePicker().pickImage(source: ImageSource.camera);
       if (image == null) return;
@@ -181,9 +138,51 @@ class _MyHomePageState extends State<MyHomePage> {
       // ignore: avoid_print, unnecessary_brace_in_string_interps
       print('Response Vehicle: ${responseJson["results"][0]["vehicle"]}');
       // ignore: avoid_print
-      final globals = responseJson["results"][0]["plate"];
+      setState(() {
+        globals = '';
+        globals = responseJson["results"][0]["plate"];
+        // ignore: avoid_print
+        print('Response plate: $globals');
+      });
       // ignore: avoid_print
-      print('Response plate: $globals');
+      // print('Response plate: $globals');
+    } on PlatformException catch (e) {
+      // ignore: avoid_print
+      print('Failed to catch image: $e');
+    }
+  }
+
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
+
+      final imageTemporary = Io.File(image.path);
+      debugPrint(imageTemporary.toString());
+      setState(() => this.image = imageTemporary);
+      final bytes = Io.File(image.path.toString()).readAsBytesSync();
+      String img64 = base64Encode(bytes);
+      debugPrint(image.path.toString());
+      var url = Uri.parse('https://api.platerecognizer.com/v1/plate-reader/');
+      var response = await http.post(url, body: {
+        'upload': img64,
+      }, headers: {
+        "Authorization": "Token 497717d5ef388c529bc0eaa84f6924baa3a4acbf"
+      });
+      // ignore: avoid_print
+      print('Response status: ${response.statusCode}');
+      // ignore: avoid_print
+      print('Response body: ${response.body}');
+      final responseJson = json.decode(response.body);
+      // ignore: avoid_print, unnecessary_brace_in_string_interps
+      print('Response Vehicle: ${responseJson["results"][0]["vehicle"]}');
+      // ignore: avoid_print
+      setState(() {
+        globals = '';
+        globals = responseJson["results"][0]["plate"];
+        // ignore: avoid_print
+        print('Response plate: $globals');
+      });
     } on PlatformException catch (e) {
       // ignore: avoid_print
       print('Failed to catch image: $e');
